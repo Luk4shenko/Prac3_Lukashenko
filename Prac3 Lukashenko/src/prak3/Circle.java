86a8ceef8febc9714fc4f6a16b825f62167639a1
Luk4shenko <zushdesign@yandex.ru>
2021-11-29 10:39:15 -0800

    Prac3

---
+++ Prac3 Lukashenko/src/prak3/Circle.java
@@ -0,0 +1,23 @@
+package prak3;
+
+public class Circle extends Shape {
+    private final double radius;
+    final double pi = Math.PI;
+
+    public Circle() {
+        this(1);
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        // A = π r^2
+        return pi * Math.pow(radius, 2);
+    }
+
+    public double perimeter() {
+        // P = 2πr
+        return 2 * pi * radius;
+    }
+}
---
+++ Prac3 Lukashenko/src/prak3/Shape.java
@@ -0,0 +1,6 @@
+package prak3;
+
+public abstract class Shape {
+    public abstract double area();
+    public abstract double perimeter();
+}
